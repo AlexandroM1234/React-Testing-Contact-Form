8a2afa9b0be07add63fe88c0614cd900ce69e699
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-12 16:58:31 -0400

    studied the form and starting the npm test

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { render, fireEvent } from "@testing-library/react";
+
+import ContactForm from "./ContactForm";
+
+test("Contact form renders correctly", () => {
+  render(<ContactForm />);
+});
